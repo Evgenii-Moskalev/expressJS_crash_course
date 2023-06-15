60356d50faa8b6f8b4233033d8375530c57200fa
Evgenii Moskalev <evgenii.moskalev.cohort2211@gmail.com>
2023-06-15 16:22:08 -0700

    Feat: added express-handlebars

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
-
+const { engine } = require('express-handlebars');
 const logger = require('./middleware/logger.js');
+const members = require('./Members.js')
+
 
 
 const app = express();
@@ -11,16 +13,29 @@ const PORT = process.env.PORT || 5000;
 // Initialize middleware
 // app.use(logger);
 
+// Handlebars middleware
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
 // Body Parser Middleware initializing
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
+
+//Homepage Route 
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member App',
+    // members: members
+    members
+}));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // });
 
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Member API Routes
 app.use('/api/members', require('./routes/api/members'));
