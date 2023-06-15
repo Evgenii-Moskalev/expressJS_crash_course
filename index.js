b6d274032f70c445edfd2f5ac0809726f1a2f506
Evgenii Moskalev <evgenii.moskalev.cohort2211@gmail.com>
2023-06-14 20:44:52 -0700

    Refactor: create routes through express.Router

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const path = require('path');
 
+const logger = require('./middleware/logger.js');
+
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+
+// Initialize middleware
+// app.use(logger);
 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -11,4 +18,6 @@ const PORT = process.env.PORT || 5000;
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, ()=> console.log(`Server started on port: ${PORT}`));
+app.use('/api/members', require('./routes/api/members'));
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
